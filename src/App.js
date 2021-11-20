c5a374783999f4e0e53ba5f78772b7f96c7471bd
I-the-Creator <andrey.burluckiy@gmail.com>
2021-11-20 15:56:59 +0100

    final version, displayed only preset array

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import data from './data';
 import List from './List';
 
 function App() {
+const [people, setPeople] = useState(data); // data as default, send array to people state
+
   return <main>
     <section className="container">
-      <h3>0 birthdays today</h3>.
-      <List />
-      <button onClick = {()=> console.log('you clicked me')}>Clear all</button>
+      <h3>{people.length} birthdays today</h3>  {/* get the array length from data.js */}
+      <List people={people} />   {/* sending people prop to List component */}
+      <button onClick = {()=> setPeople([])}>Clear all</button>  {/* clear people by assign empty array [] using setPeople */}
     </section>
   </main>
 }
--- src/List.js
+++ src/List.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const List = () => {
-  return (
-    <>
-      <h2>list component</h2>
-    </>
-  );
+
+const List = ({ people }) => {  /* destructure function parameter, looking for and getting people prop from App  */
+    return (
+        <>
+            {people.map((person)=>{  /* iterate on people prop (array got from App - data) */
+                const {id, name, age, image} = person;  // destructure params of person 
+                return <article key={id} className="person">
+                    <img src={image} alt={name}/>
+                    <div>
+                        <h4>{name}</h4>
+                        <p>{age} years</p>
+                    </div>
+                </article>
+            })}
+        </>
+    );
 };
 
 export default List;
